671b5aceec2970c3434499fc9370292cb7c3e1c1
Olga Benchuk <olga.benchuk@check24.de>
2024-12-15 14:38:41 +0100

    fix

--- tests/smoke.spec.js
+++ tests/smoke.spec.js
@@ -11,5 +11,5 @@ test.beforeEach(async ({ page }) => {
 });
 
 test("see the main page", async ({ page }) => {
-  await expect(page.locator(".hero__title.heroTitle_ohk")).toBeVisible();
+  await expect(page.locator(".hero__title.heroTitle_o")).toBeVisible();
 });
